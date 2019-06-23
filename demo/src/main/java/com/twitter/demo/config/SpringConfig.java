d27a3987c465295d2d882909a77795755bf0f20d
Artur <akoczaja@gmail.com>
2019-06-23 15:48:59 +0200

    password

--- demo/src/main/java/com/twitter/demo/config/SpringConfig.java
+++ demo/src/main/java/com/twitter/demo/config/SpringConfig.java
@@ -31,7 +31,7 @@ public class SpringConfig {
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/twitter?allowPublicKeyRetrieval=true&useSSL=false");
         dataSource.setUsername("root");
-        dataSource.setPassword("Barcelona19");
+        dataSource.setPassword("arturk15");
         return dataSource;
     }
 
